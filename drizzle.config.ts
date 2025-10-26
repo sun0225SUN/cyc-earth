307c915f2584501536b1f27c561a5ba4a3d627e5
Guoqi Sun <hi@guoqi.dev>
2025-10-26 19:26:09 +0800

    feat: support turso database connection, upgrade dependency version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,20 @@
-import type { Config } from "drizzle-kit";
+import type { Config } from 'drizzle-kit'
 
-import { env } from "@/env";
+import { env } from '@/env'
+
+const dbCredentials =
+  env.DATABASE_PROVIDER === 'turso'
+    ? {
+        url: env.TURSO_DATABASE_URL!,
+        authToken: env.TURSO_DATABASE_TOKEN!,
+      }
+    : {
+        url: env.DATABASE_URL!,
+      }
 
 export default {
-	schema: "./src/server/db/schema.ts",
-	dialect: "sqlite",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
-	tablesFilter: ["cyc_*"],
-} satisfies Config;
+  schema: './src/server/db/schema.ts',
+  dialect: env.DATABASE_PROVIDER,
+  dbCredentials,
+  tablesFilter: ['cyc_earth_*'],
+} satisfies Config
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,29 @@
-import { type Client, createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { type Client, createClient } from '@libsql/client'
+import { drizzle } from 'drizzle-orm/libsql'
 
-import { env } from "@/env";
-import * as schema from "./schema";
+import { env } from '@/env'
+import * as schema from './schema'
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-	client: Client | undefined;
-};
+  client: Client | undefined
+}
 
 export const client =
-	globalForDb.client ?? createClient({ url: env.DATABASE_URL });
-if (env.NODE_ENV !== "production") globalForDb.client = client;
+  env.DATABASE_PROVIDER === 'turso'
+    ? (globalForDb.client ??
+      createClient({
+        url: env.TURSO_DATABASE_URL!,
+        authToken: env.TURSO_DATABASE_TOKEN!,
+      }))
+    : (globalForDb.client ??
+      createClient({
+        url: env.DATABASE_URL!,
+      }))
 
-export const db = drizzle(client, { schema });
+if (env.NODE_ENV !== 'production') globalForDb.client = client
+
+export const db = drizzle(client, { schema })
